5ca7511456dc469242dfcd99226c00b0c5959923
jasonbasuil <basuilj@gmail.com>
2020-10-13 12:03:44 -0700

    solves Dark Mode Challenge

--- rocket-ship/src/routes/LaunchPad/components/Rocket/components/Rocket.js
+++ rocket-ship/src/routes/LaunchPad/components/Rocket/components/Rocket.js
@@ -1,10 +1,10 @@
-import React, { useState, Component } from 'react';
-import RocketCore from './RocketCore';
+import React, { useState, Component } from "react";
+import RocketCore from "./RocketCore";
 
 export function FunctionalRocket() {
   const [initialLaunchTime] = useState(Date.now());
 
-  return <RocketCore initialLaunchTime={initialLaunchTime} />;
+  return <RocketCore />;
 }
 
 export class ClassRocket extends Component {
@@ -12,13 +12,13 @@ export class ClassRocket extends Component {
     super();
 
     this.state = {
-      initialLaunchTime: Date.now()
+      initialLaunchTime: Date.now(),
     };
   }
 
   render() {
     const { initialLaunchTime } = this.state;
 
-    return <RocketCore initialLaunchTime={initialLaunchTime} />;
+    return <RocketCore />;
   }
 }
